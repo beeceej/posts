659476e62768719eb473f8fcca6d92d6ff3d353e
Brian Jones <beeceej.code@gmail.com>
2019-07-06 18:02:31 -0500

    fix: terraform fmt, fix policy

--- deployment/infrastructure/modules/static_hosting_s3_bucket/main.tf
+++ deployment/infrastructure/modules/static_hosting_s3_bucket/main.tf
@@ -4,9 +4,10 @@ variable "bucket_policy_json" {}
 resource "aws_s3_bucket" "static" {
   bucket = "${var.bucket_name}"
   acl    = "public-read"
+  policy = "${var.bucket_policy_json}"
 
   website {
     index_document = "index.html"
-    policy = "${var.bucket_policy_json}"
+
   }
 }
