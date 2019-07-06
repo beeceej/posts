9adc1946697179ceabecc3e20a76d51a68afae64
Brian Jones <beeceej.code@gmail.com>
2019-07-06 17:36:24 -0500

    create new static bucket

--- deployment/dev/provider.tf
+++ deployment/dev/provider.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+provider "cloudflare" {
+  email = "jonesbrianc26@gmail.com"
+  token = "${var.cloudflare_token}"
+}
+
 terraform {
   backend "s3" {
     bucket = "dev-beeceej-ops"
