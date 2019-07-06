7756db6336a5a35c9afefda51bffd7f770d33959
Brian Jones <beeceej.code@gmail.com>
2019-07-06 00:32:12 -0500

    tmp test

--- deployment/envdev/provider.tf
+++ deployment/envdev/provider.tf
@@ -1,19 +1,19 @@
 provider "aws" {
   region  = "us-east-1"
-  profile = "terraformx"
-  assume_role {
-    role_arn     = "arn:aws:iam::782123507683:role/admin"
-    session_name = "terraform"
-  }
+  # profile = "terraformx"
+  # assume_role {
+  #   role_arn     = "arn:aws:iam::782123507683:role/admin"
+  #   session_name = "terraform"
+  # }
 }
 
 terraform {
   backend "s3" {
-    profile  = "terraformx"
+    # profile  = "terraformx"
     bucket   = "dev-beeceej-ops"
     key      = "remote-state/pipeline/terraform.tfstate"
     region   = "us-east-1"
-    role_arn = "arn:aws:iam::782123507683:role/admin"
+    # role_arn = "arn:aws:iam::782123507683:role/admin"
   }
 }
 
